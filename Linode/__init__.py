a7d8d3c32961ee694ba51aa3d213d80137551276
Empty <77683874+MrX0955@users.noreply.github.com>
2022-08-28 00:37:54 +0300

    Create __init__.py

---
+++ Linode/__init__.py
@@ -0,0 +1,28 @@
+import cfscrape
+from bs4 import BeautifulSoup
+
+def linode(username,password):
+    scraper = cfscrape.create_scraper()
+
+    #cookie_arg, user_agentt = cfscrape.get_cookie_string("https://login.linode.com/login")
+
+    req = scraper.get("https://login.linode.com/login")
+    soup = BeautifulSoup(req.content, 'html.parser')
+    title = soup.find(type="hidden")
+    TK = title.attrs.get('value')
+
+    datas = f"csrf_token={TK}&return_to=https%3A%2F%2Flogin.linode.com%2F&created=2021-03-21T07%3A43%3A43.668635&username={username}&password={password}&submit=Oturum+a%C3%A7"
+
+    headerss = {
+        "Accept": "*/*",
+        "Content-Type": "application/x-www-form-urlencoded"
+    }
+
+    req = scraper.post("https://login.linode.com/login", data=datas, headers=headerss, allow_redirects=False).text
+    print(req)
+    if "<li>Username or password incorrect.</li>" in req:
+        print(f"Wrong Credentials >> {username}:{password}")
+    elif "You should be redirected automatically to target URL: <a href=\"https://login.linode.com/\">https://login.linode.com/</a>." in req:
+        print(f"Login Successfully! >> {username}:{password}")
+    elif "You should be redirected automatically to target URL: <a href=\"/login/verify\">/login/verify</a>." in req:
+        print(f"2FACTOR >> {username}:{password}")
